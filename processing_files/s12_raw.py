8d6f106f1053ee9710f45109d7e67632e38a4cc6
abastos <abastos@abastos_server.com>
2020-07-29 15:05:43 -0500

    logging outputs

--- processing_files/s12_raw.py
+++ processing_files/s12_raw.py
@@ -25,12 +25,21 @@ def connect():
 
 def s12_listen(ser):
     """Listens for the answer of the S12 and returns the result"""
+    # logging all outputs
+    try:
+        log_file = open('data/output/s12_raw.log', 'a')
+    except:
+        print('Error en la estructura de los archivos, descarga de nuevo el proyecto.')
+        print('Para descargarlo: git clone https://github.com/sergioCocodrilo/S12_raw.git')
+        quit()
     output_ended = False
     while not output_ended:
         for line in ser.readlines():
             print(line[:-1].decode("ascii"))
+            log_file.write(line.decode('ascii'))
             if b"<" in line or b">" in line:
                 output_ended = True 
+    log_file.close()
     
 if __name__ == "__main__":
     ser = connect()
